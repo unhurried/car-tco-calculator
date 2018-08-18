c5822d17928122d1936a498e436c85eb4b50c8dc
unhurried <unhurried@users.noreply.github.com>
2018-08-18 18:15:21 +0900

    PWA対応

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from "./store.js";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pwa: {
+    name: '自動車TCO見積りツール',
+    themeColor: '#646464',
+    msTileColor: '#00aba9'
+  }
+}
